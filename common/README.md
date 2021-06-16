0903902318b0c15fe8ea7a717f862d9a9a3f5b21
apolkingg8 <apolkingg8@gmail.com>
2021-06-16 16:30:58 +0800

    :memo: common module document

---
+++ common/README.md
@@ -0,0 +1,6 @@
+# Common modules
+Application common modules
+
+## Development
+* Modules can be run in both `render` side and `main` side.
+* Modules should be tested in both `render` and `main` environment.
---
+++ common/example.test.ts
@@ -0,0 +1,24 @@
+
+describe("example", ()=> {
+
+    test("constructor()", async ()=> {
+
+    })
+
+    test("foo()", async ()=> {
+
+    })
+
+    test("foo() bad input", async ()=> {
+
+    })
+
+    test("bar()", async ()=> {
+
+    })
+
+    test("bar() bad input", async ()=> {
+
+    })
+
+})
